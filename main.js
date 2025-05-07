ab114c6a0b9ae223f1cce421ce7b76516221c1c4
Taha Mehjabin <marvime660@gmail.com>
2025-05-07 12:59:24 +0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+  VANTA.TRUNK({
+    el: "#trunk-heading",
+    mouseControls: true,
+    touchControls: true,
+    scale: 1.0,
+    scaleMobile: 1.0,
+    color: 0xffffff,
+    backgroundColor: 0x0a0a0a
+  });
+
+  // VANTA Birds for the rest of the page
+  VANTA.BIRDS({
+    el: "#background",
+    mouseControls: true,
+    touchControls: true,
+    minHeight: 200.00,
+    minWidth: 200.00,
+    scale: 1.0,
+    scaleMobile: 1.0,
+    backgroundColor: 0x0a0a23,
+    color1: 0xffcc00,
+    color2: 0xff0099,
+    birdSize: 1.5,
+    wingSpan: 25
+  });
